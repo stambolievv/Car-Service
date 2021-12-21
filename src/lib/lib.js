5a0c1d3ce21c08bb1b8e75846413d60097050391
stambolievv <deyan.stamboliev@gmail.com>
2021-12-21 13:29:40 +0200

    add cdn

--- src/lib/lib.js
+++ src/lib/lib.js
@@ -1,6 +1,10 @@
-import { html, render, nothing } from '../../node_modules/lit-html/lit-html.js';
-import { until } from '../../node_modules/lit-html/directives/until.js';
-import page from '../../node_modules/page/page.mjs';
+// import { html, render, nothing } from '../../node_modules/lit-html/lit-html.js';
+// import { until } from '../../node_modules/lit-html/directives/until.js';
+// import page from '../../node_modules/page/page.mjs';
+
+import { html, render, nothing } from '//unpkg.com/lit-html?module';
+import { until } from 'https://unpkg.com/lit-html/directives/untul.js?module';
+import page from '//unpkg.com/page/page.mjs';
 
 export {
     html,
