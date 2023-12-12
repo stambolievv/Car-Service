e4da430da01ee8f849bd5684e1e83093ed969c3e
Deyan Stamboliev <deyan.stamboliev@gmail.com>
2023-12-12 23:27:12 +0200

    refactor: use session storage instead of local storage

--- src/api/services/userService.js
+++ src/api/services/userService.js
@@ -5,7 +5,7 @@ import { memoization, updateNavigation } from '../../utilities';
 const USER_STORAGE_KEY = 'car-service-current-user-data';
 
 /**
- * @description Performs a user login operation with the provided user credentials. Upon successful authentication, the user's information is stored in the local storage and the navigation bar is updated.
+ * @description Performs a user login operation with the provided user credentials. Upon successful authentication, the user's information is stored in the session storage and the navigation bar is updated.
  * @param {UserLoginCredentials} data - User credentials.
  * @returns {Promise<UserAuthData>} The result from the server.
  */
@@ -19,7 +19,7 @@ export async function login(data) {
 }
 
 /**
- * @description Performs a user registration operation with the provided user credentials. Upon successful authentication, the user's information is stored in the local storage and the navigation bar is updated.
+ * @description Performs a user registration operation with the provided user credentials. Upon successful authentication, the user's information is stored in the session storage and the navigation bar is updated.
  * @param {UserLoginCredentials} data - User credentials.
  * @returns {Promise<UserAuthData>} The result from the server.
  */
@@ -33,7 +33,7 @@ export async function register(data) {
 }
 
 /**
- * @description Performs a user logout operation. It removes the user data stored in the local storage and updates the navigation accordingly.
+ * @description Performs a user logout operation. It removes the user data stored in the session storage and updates the navigation accordingly.
  * @returns {Promise<{}>} The result from the server.
  */
 export async function logout() {
@@ -46,34 +46,34 @@ export async function logout() {
 }
 
 /**
- * @description Checks whether user data exists in the local storage.
+ * @description Checks whether user data exists in the session storage.
  * @returns {boolean} Returns true if user data is present, otherwise false.
  */
 export function hasUserData() {
-  return !!localStorage.getItem(USER_STORAGE_KEY);
+  return !!sessionStorage.getItem(USER_STORAGE_KEY);
 }
 
 /**
- * @description Retrieves user data from the local storage.
+ * @description Retrieves user data from the session storage.
  * @returns {UserStoredData | null} Returns the parsed user data if it exists, otherwise returns null.
  */
 export function getUserData() {
-  return JSON.parse(localStorage.getItem(USER_STORAGE_KEY) ?? 'null');
+  return JSON.parse(sessionStorage.getItem(USER_STORAGE_KEY) ?? 'null');
 }
 
 /**
- * @description Sets user data in the local storage.
- * @param {UserStoredData} data - The data to be stored in the local storage.
+ * @description Sets user data in the session storage.
+ * @param {UserStoredData} data - The data to be stored in the session storage.
  */
 export async function setUserData(data) {
   await memoization.deleteCache().catch(console.error);
-  localStorage.setItem(USER_STORAGE_KEY, JSON.stringify(data));
+  sessionStorage.setItem(USER_STORAGE_KEY, JSON.stringify(data));
 }
 
 /**
- * @description Removes user data from the local storage.
+ * @description Removes user data from the session storage.
  */
 export async function removeUserData() {
   await memoization.deleteCache().catch(console.error);
-  localStorage.removeItem(USER_STORAGE_KEY);
+  sessionStorage.removeItem(USER_STORAGE_KEY);
 }
